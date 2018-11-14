89d6743137765a03dd0c1d6f544b7661c34a5721
nreje <niklas.reje@gmail.com>
2018-11-14 22:30:45 +0000

    Improved overflow that works in minix as you would expect with added DEBUG and pointer prints

--- games/overflow/overflow.c
+++ games/overflow/overflow.c
@@ -2,38 +2,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char* text;
-char* secret = "THIS IS A SECRET";
 char* secret2;
+char* text;
+const char* secret = "THIS IS A SECRET";
 
 /*Not supposed to be read*/
-void readSecret(){
+static void readSecret(){
 	printf("THE SECRET IS OUT: \"%s\"\n",secret);
 }
 
 /*Mismatch between array size of text and strncpy limit (heap)*/
-void enterText(char *te){
-	strncpy(text,te,32);
+static void enterText(char *te,int n){
+	printf("DEBUG: int: %d\n",n);
+	strncpy(text,te,n);
 }
 
 /*Can overwrite super secure check array (stack)*/
-void enterTest(char *te,char *comp){
-	char test[16];
+static void enterTest(char *te,char *comp){
 	char check[16];
+	char test[16];
+	printf("POINTERS STACK: test: %p check: %p\n",(void *)&test,(void *)&check);
+	printf("POINTERS STACK: diff: %p\n",(void *)((&check)-(&test)));
 	strcpy(check,"UNCRACKABLE");
 	strcpy(test,te);
 	if(!strcmp(comp,check)) readSecret();
 }
 
+/*4 Inputs*/
 int main(int argc, char **argv){
-	text = (char*) malloc(16*sizeof(char));
+	text = (char*) malloc(32*sizeof(char));
 	secret2 = (char*) malloc(32*sizeof(char));
+
+	printf("POINTERS HEAP: text: %p secret2: %p\n",(void*) text,(void *) secret2);
+	printf("POINTERS HEAP: diff: %p\n",(void *)(secret2 - text));
 
 	strcpy(secret2,"THIS IS ANOTHER SECRET");
 	enterTest(argv[1],argv[2]);
-	enterText(argv[3]);
+	enterText(argv[3],atoi(argv[4]));
 
-	printf("%s\n",text);
-	/*printf("DEBUG: %s\n",secret2);*/
+	printf("read text: %s\n",text);
+	printf("DEBUG secret2: %s\n",secret2);
 }
 
