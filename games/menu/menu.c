a4972f38a341a0149b6090e765483f13cd8282d7
Dandor <vidarpal@gmail.com>
2018-11-21 14:11:55 +0100

    menu program to test mpatch

---
+++ games/menu/menu.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int fibon(){
+	printf("Called fib\n");
+	return 0;
+}
+
+int printConst(){
+	printf("A constant string\n");
+	return 0;
+}
+
+int main(){
+	char input[100];
+	while(1){
+		printf("write input\n");
+		scanf("%s", input);
+		if(!strncmp(input, "const", 5)){
+			printConst();
+		}
+		if(!strncmp(input, "fib", 3)){
+			fibon();
+		}
+		if(!strncmp(input, "quit", 4)){
+			break;
+		}
+	}
+}
